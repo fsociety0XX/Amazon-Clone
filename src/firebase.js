361bd5686f3526e5532f241b525ce79b1394ded6
Kushagra <kushagra@o2h.com>
2020-09-13 19:00:16 +0530

    frontend part in prog header section done

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA1ZsKZSphYdRVz48aerHaQWROGSdN_s4o",
+  authDomain: "clone-8d82d.firebaseapp.com",
+  databaseURL: "https://clone-8d82d.firebaseio.com",
+  projectId: "clone-8d82d",
+  storageBucket: "clone-8d82d.appspot.com",
+  messagingSenderId: "373558782505",
+  appId: "1:373558782505:web:264e9871c28f297e840a38",
+};
